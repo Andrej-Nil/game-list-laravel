c5a3af301e5337207463fdbf3a25ff36e423d16f
AndrejNil <and.kucheroff@yandex.ru>
2022-11-06 23:48:21 +0500

    Внес изменение на главную стр. Создал страницу со списком игр. Немного напимал логитку

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,41 @@
-console.log('hello');
+"use strict";
+class UserMenu {
+  constructor() {
+    this.$userMenu = document.querySelector('#userMenu');
+    this.init();
+  }
+
+  init = () => {
+    if (!this.$userMenu) return;
+    this.$userMenuBtn = this.$userMenu.querySelector('[data-user-menu-btn]');
+    this.$userMenuList = this.$userMenu.querySelector('[data-user-menu-list]');
+    this.listeners();
+  }
+
+
+  open = () => {
+    this.$userMenuList.classList.remove('user-menu__list_close');
+    this.$userMenuList.dataset.userMenuList = 'open';
+  }
+
+  close = () => {
+    this.$userMenuList.classList.add('user-menu__list_close');
+    this.$userMenuList.dataset.userMenuList = 'close';
+  }
+
+  toggleUserMenu = () => {
+    const statusMenu = this.$userMenuList.dataset.userMenuList;
+    if (statusMenu === 'close') {
+      this.open()
+    } else {
+      this.close()
+    }
+  }
+
+  listeners = () => {
+    this.$userMenuBtn.addEventListener('click', this.toggleUserMenu)
+  }
+}
+
+const userMenu = new UserMenu();
 
